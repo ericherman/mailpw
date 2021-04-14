6d038f1a23519129179a07ab04ad11a626640b80
Eric Herman <eric@freesa.org>
2021-04-14 07:34:46 +0000

    GPL-3.0-or-later

--- tests/expect-no-confirm.sh
+++ tests/expect-no-confirm.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/expect
+# SPDX-License-Identifier: GPL-3.0-or-later
+# Copyright (C) 2021 Eric Herman <eric@freesa.org>
+# Copyright (C) 2021 Keith Reynolds <keithr@pwcrypt.keithr.com>
 set type [lindex $argv 0]
 set algo [lindex $argv 1]
 set salt [lindex $argv 2]
